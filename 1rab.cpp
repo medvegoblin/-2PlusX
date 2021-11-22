4b0c9092f694cf7a3c969f72fa4a71f9133bade4
medvegoblin <medvegoblin1@yandex.ru>
2021-11-23 00:17:02 +0300

    Create 1rab.cpp

---
+++ 1rab.cpp
@@ -0,0 +1,119 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+   
+   int a1=3;
+   int a2=1;
+   int a3=4;
+   int a4=1;
+   int a5=5;
+   int a6=9;
+   int a7=2;
+   int a8=6;
+   int a9=5;
+   int a10=3;
+   
+   short a11=5;
+   short a12=8;
+   short a13=9;
+   short a14=7;
+   short a15=9;
+   short a16=3;
+   short a17=2;
+   short a18=3;
+   short a19=8;
+   short a20=4;
+   
+   long a21=6;
+   long a22=2;
+   long a23=6;
+   long a24=4;
+   long a25=3;
+   long a26=3;
+   long a27=8;
+   long a28=3;
+   long a29=2;
+   long a30=79502;
+   
+   char c='c';
+   char i='i';
+   char s='s';
+   char l='l';
+   char o='o';
+   char p='p';
+   char h='h';
+   char space=' ';
+   char zap=',';
+   char rav='=';
+    
+    float b1 = 12.51;
+    float b2 = 5.12;
+    float b3 = 2.89;
+    float b4 = 5.831;
+    float b5 = 7.54;
+    float b6 = 23.1;
+    float b7 = 135.643;
+    float b8 = 14.15;
+    float b9 = 23.125;
+    float b10 =15.152;
+    
+    double c1 =125.25;
+    double c2 =3.2;
+    double c3 =775.12235;
+    double c4 =2315.1356;
+    double c5 =332.153676;
+    double c6 =3564.36;
+    double c7 =686.264;
+    double c8 =32671.1855;
+    double c9 =43875.29115;
+    double c10 =15793.19753;
+    
+    bool d1 = true;
+    bool d2 = true;
+    bool d3 = false;
+    bool d4 = false;
+    bool d5 = true;
+    bool d6 = false;
+    bool d7 = true;
+    bool d8 = true;
+    bool d9 = false;
+    bool d10 = false;
+
+   cout <<endl<<endl<<c<<h<<i<<s<<l<<o<<space<<p<<i<<space<<rav<<space<<a1<<zap<<a2<<a3<<a4<<a5<<a6<<a7<<a8
+   <<a9<<a10<<a11<<a12<<a13<<a14<<a15<<a16<<a16<<a18<<a19<<a20<<a21<<a22<<a23<<a24
+   <<a25<<a26<<a27<<a28<<a29<<a30<<endl<<endl;
+    cout <<b1<<endl;
+    cout <<b2<<endl;
+    cout <<b3<<endl;
+    cout <<b4<<endl;
+    cout <<b5<<endl;
+    cout <<b6<<endl;
+    cout <<b7<<endl;
+    cout <<b8<<endl;
+    cout <<b9<<endl;
+    cout <<b10<<endl<<endl;   
+    cout <<c1<< endl;
+    cout <<c2<< endl;
+    cout <<c3<< endl;
+    cout <<c4<< endl;
+    cout <<c5<< endl;
+    cout <<c6<< endl;
+    cout <<c7<< endl;
+    cout <<c8<< endl;
+    cout <<c9<< endl;
+    cout <<c10<< endl << endl;
+    cout << d1<< endl;
+    cout <<d2<< endl;
+    cout <<d3<< endl;
+    cout <<d4<< endl;
+    cout <<d5<< endl;
+    cout <<d6<< endl;
+    cout <<d7<< endl;
+    cout <<d8<< endl;
+    cout <<d9<< endl;
+    cout <<d10<<endl << endl;
+    return 0;
+}
